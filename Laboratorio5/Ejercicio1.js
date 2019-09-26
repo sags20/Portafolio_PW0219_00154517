1e7b12070c1aac84e1f74c8c86e2cf2f9635151c
sags20 <00154517@uca.edu.sv>
2019-09-26 14:20:15 -0600

    ejercicio1

--- Laboratorio5/Ejercicio1.js
+++ Laboratorio5/Ejercicio1.js
@@ -1,7 +1,8 @@
 /*Realice una función que reciba un arreglo como parámetro y liste los tipos de datos de sus 
 elementos y cuantos de cada tipo. */
 
-var auno=[]; 
+var auno=['hola','dos'];
+
 function uno(auno){
-    console.log(typeof auno);
+console.log(typeof auno);
 }
--- Laboratorio5/Ejercicio4.js
+++ Laboratorio5/Ejercicio4.js
@@ -1,3 +1,7 @@
+/*Elabore una función que llene un arreglo con 20 números de forma aleatoria (random),
+ y que pida al usuario adivinar un número, si lo adivina mostrará un mensaje indicando que ganó,
+  si no mostrará que perdió. */
+
 function cuatro(numero){
     valores=[];
     validacion=false;
