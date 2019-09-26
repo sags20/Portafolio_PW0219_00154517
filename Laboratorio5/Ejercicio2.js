f00ec84c8c9b4c8427cbd35a4bc5dee72d5fc1ed
sags20 <00154517@uca.edu.sv>
2019-09-26 14:54:06 -0600

    fix:Ejercico2

--- Laboratorio5/Ejercicio2.js
+++ Laboratorio5/Ejercicio2.js
@@ -5,11 +5,12 @@
  var numero = Array();
  var suma=0;
  var promedio=0;
-
-
+function name(Array) {
+    
  for (var i = 0; i < 4; i++){
      numero[i] = Number();				
      suma = suma + numero[i]; 
  }
  
 console.log('suma = ' +suma + '; promedio = ' + suma/Array.length);
+}
---
+++ Laboratorio5/Ejercicio3.js
@@ -0,0 +1,8 @@
+/*Realice una función en JS, que dados un número y un arreglo como parámetros, 
+retorne cuantas ocurrencias existen de ese número dentro del arreglo. */
+
+
+var arr =Array[];
+var numero= ;
+
+function ocurrencias()
