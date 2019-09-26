204cd5a94460688df796bf63561e434231075397
sags20 <00154517@uca.edu.sv>
2019-09-26 15:21:21 -0600

    add:Ejercicio6

---
+++ Laboratorio5/Ejercicio6.js
@@ -0,0 +1,10 @@
+/*Ordenamiento de array: Crear una función en JavaScript que ordene 
+los elementos de un Array en orden ascendente. */
+
+
+var numeros=Array[];
+
+function arreglo(numeros){
+    numeros.sort();
+    
+}
