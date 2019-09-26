decfe435a02d9a8ed9e474feb0ad2359a9eb86a6
sags20 <00154517@uca.edu.sv>
2019-09-26 14:46:09 -0600

    add:ejercicio2

--- Laboratorio5/Ejercicio2.js
+++ Laboratorio5/Ejercicio2.js
@@ -1,4 +1,15 @@
 /*Realice una función que reciba un arreglo de datos numéricos,
  y muestre la suma de todos los elementos del arreglo y su promedio. */
 
+
+ var numero = Array();
+ var suma=0;
+ var promedio=0;
+
+
+ for (var i = 0; i < 4; i++){
+     numero[i] = Number();				
+     suma = suma + numero[i]; 
+ }
  
+console.log('suma = ' +suma + '; promedio = ' + suma/Array.length);
