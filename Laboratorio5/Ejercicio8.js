335a1cc642853e3bb247e145d11c090af5a18405
sags20 <00154517@uca.edu.sv>
2019-09-26 15:31:26 -0600

    add:Ejercicio8

---
+++ Laboratorio5/Ejercicio8.js
@@ -0,0 +1,7 @@
+/*Conversor decimal-binario: Crear una función en JavaScript, que reciba como parámetro un entero positivo. 
+La función debe devolver el valor en binario. */
+
+ let decimal = 324; 
+ 
+ let decimalEnBinario = decimal.toString(2); 
+ console.log("El número decimal %s en binario es %s", decimal, decimalEnBinario);
